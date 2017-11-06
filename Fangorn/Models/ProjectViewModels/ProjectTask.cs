6beb5aee28dbe86c4e772d6d3b80029e7a9afc14
Matthew Dolan <mdolan2424@gmail.com>
2017-11-05 21:00:53 -0700

    Fixed issue with project ID not displaying correctly on details view.

--- Fangorn/Models/ProjectViewModels/ProjectTask.cs
+++ Fangorn/Models/ProjectViewModels/ProjectTask.cs
@@ -10,7 +10,7 @@ namespace Tower.Models.ProjectViewModels
 {
     public class ProjectTask
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         [Required]
         [MaxLength(100)]
         public string Title { get; set; }
