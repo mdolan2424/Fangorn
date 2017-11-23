3de5f8a7dee2b0a888355da0e82deb5647be26a3
Matthew Dolan <mdolan2424@users.noreply.github.com>
2017-11-22 17:22:27 -0700

    Nov22

---
+++ Fangorn/Models/ServiceOrderViewModels/BillableViewModels/BillableTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tower.Models.ClientViewModels;
+
+namespace Tower.Models.ServiceOrderViewModels.BillableViewModels
+{
+    public class BillableTime
+    {
+        public int Id { get; set; }
+        public int Minutes { get; set; }
+        public String WorkPerformed { get; set; }
+        public ServiceOrder ServiceOrder { get; set; }
+        public ApplicationUser AddedBy { get; set; }
+    }
+}
--- Fangorn/Models/ServiceOrderViewModels/ServiceOrderCommentsViewModel.cs
+++ Fangorn/Models/ServiceOrderViewModels/ServiceOrderCommentsViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tower.Models.ServiceOrderViewModels.BillableViewModels;
 
 namespace Tower.Models.ServiceOrderViewModels
 {
@@ -11,5 +12,6 @@ namespace Tower.Models.ServiceOrderViewModels
         public ServiceOrder ServiceOrder { get; set; }
         public Comment Comment { get; set; }
         public List<Comment> Comments { get; set; }
+        public List<BillableTime> BillableTimes { get; set; }
     }
 }
