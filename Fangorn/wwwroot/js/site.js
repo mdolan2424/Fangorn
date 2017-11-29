aa91dd90eacea1a24f5daf541d1ef5cece6f289c
Matthew Dolan <mdolan2424@users.noreply.github.com>
2017-11-28 20:49:28 -0700

    Finalized sidebar. Allowed sidebar to minimize on mobile screens.

--- Fangorn/wwwroot/js/site.js
+++ Fangorn/wwwroot/js/site.js
@@ -1,1 +1,11 @@
 ﻿
+$(document).ready(function () {
+    function openNav() {
+
+        document.getElementByClassName("sidebar").style.width = "200px";
+    }
+
+        function closeNav() {
+    document.getElementByClassName("sidebar").style.width = "0";
+}
+    });
